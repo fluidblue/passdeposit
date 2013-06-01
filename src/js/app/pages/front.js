a0773e25860df8ed84e520eb0a642011dce7889b
fluidblue <fluidblue@github>
2013-06-01 13:18:22 +0200

    Added typeahead

--- src/js/app/pages/front.js
+++ src/js/app/pages/front.js
@@ -159,5 +159,22 @@ function($, domReady)
 		$("#login").submit(loginUser);
 		$("#loginPassForgotten").click(fnNotImpl);
 		$("#register").submit(fnNotImpl);
+		
+		// TODO: Move to main.js
+		var options =
+		{
+			source: ["Alabama","Alaska","Arizona","Arkansas","California",
+				"Colorado","Connecticut","Delaware","Florida","Georgia",
+				"Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas",
+				"Kentucky","Louisiana","Maine","Maryland","Massachusetts",
+				"Michigan","Minnesota","Mississippi","Missouri","Montana",
+				"Nebraska","Nevada","New Hampshire","New Jersey","New Mexico",
+				"New York","North Dakota","North Carolina","Ohio","Oklahoma",
+				"Oregon","Pennsylvania","Rhode Island","South Carolina",
+				"South Dakota","Tennessee","Texas","Utah","Vermont",
+				"Virginia","Washington","West Virginia","Wisconsin","Wyoming"]
+		};
+		
+		$("#search").typeahead(options);
 	});
 });
