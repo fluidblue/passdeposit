63f69f8ac58ecaec00b4c43ec6a4a83bc3619588
fluidblue <fluidblue@github>
2013-06-15 10:42:48 +0200

    Added lock dialog

--- src/js/pages/main.js
+++ src/js/pages/main.js
@@ -12,6 +12,27 @@ var config = require('../components/config');
 var initOptionsDialog = require('./main/options');
 var jGrowl = require('../components/jgrowl-extend');
 
+function logout()
+{
+	// TODO: Clean up data!
+	
+	// Close all jGrowl messages
+	jGrowl.closeAll();
+	
+	// Switch to frontpage
+	$('#mainpage').fadeOut(config.animations.pageChangeDuration, function()
+	{
+		$('#frontpage').fadeIn(config.animations.pageChangeDuration);
+		setFormFocus('#login');
+	});
+}
+
+function lock()
+{
+	// TODO: Hide data!
+	$('#lockDialog').modal('show');
+}
+
 // Initializes main page
 function init()
 {
@@ -19,17 +40,47 @@ function init()
 	
 	$('#btnLogout').click(function()
 	{
-		// TODO: Clean up data!
+		logout();
+	});
+	
+	$('#btnLock').click(function()
+	{
+		lock();
+		return false;
+	});
+	
+	$('#lockDialog').on('shown', function()
+	{
+		$('#lockDialog input.pass').focus();
+	});
+	
+	$('#lockDialog').submit(function()
+	{
+		// Remove password
+		$('#lockDialog input.pass').val('');
 		
-		// Close all jGrowl messages
-		jGrowl.closeAll();
+		// Close dialog
+		$('#lockDialog').modal('hide');
 
-		// Switch to frontpage
-		$('#mainpage').fadeOut(config.animations.pageChangeDuration, function()
+		return false;
+	});
+	
+	$('#lockDialog .btnLogout').click(function()
+	{
+		// Remove password, if any
+		$('#lockDialog input.pass').val('');
+		
+		// Logout after dialog closes
+		$('#lockDialog').on('hidden.logout', function()
 		{
-			$('#frontpage').fadeIn(config.animations.pageChangeDuration);
-			setFormFocus('#login');
+			$('#lockDialog').off('hidden.logout');
+			logout();
 		});
+		
+		// Close dialog
+		$('#lockDialog').modal('hide');
+
+		return false;
 	});
 
 	// TODO: Multiple typeahead
