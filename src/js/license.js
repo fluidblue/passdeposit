1e1f2abcbab61ae34dcf43fec839fbe60d3a09be
fluidblue <fluidblue@github>
2013-07-05 00:09:44 +0200

    Using full text instead of @author in license

--- src/js/license.js
+++ src/js/license.js
@@ -3,10 +3,11 @@
  * 
  * PassDeposit
  * 
- * @author Max Geissler
+ * Created by Max Geissler
  * 
  * You should have received a copy of the license along with this
  * program; if not, see <https://github.com/fluidblue/passdeposit>
  * 
  * ================================================================
  */
+ 
