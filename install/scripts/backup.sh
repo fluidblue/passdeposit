021a1de7c5e54500e77f58c7bcdfee0ebc30048a
fluidblue <fluidblue@github>
2021-08-25 19:17:17 +0200

    Added host

--- install/scripts/backup.sh
+++ install/scripts/backup.sh
@@ -1,4 +1,4 @@
 echo "Test. Password:"
 cat /etc/passdeposit/mongodb_initial_passdeposit_password.txt
 
-mongodump --db passdeposit -u passdeposit -p "$(cat /etc/passdeposit/mongodb_initial_passdeposit_password.txt)" --out /var/passdeposit/backup/mongo_backup_$(date +%Y-%m-%d)/
+mongodump --host mongodb --db passdeposit -u passdeposit -p "$(cat /etc/passdeposit/mongodb_initial_passdeposit_password.txt)" --out /var/passdeposit/backup/mongo_backup_$(date +%Y-%m-%d)/
