b2f0bef6fd2558e66f355eceec1c1101cd47d5d8
fluidblue <fluidblue@github>
2021-08-25 19:11:40 +0200

    Anacron scripts

---
+++ install/scripts/backup.sh
@@ -0,0 +1,4 @@
+echo "Test. Password:"
+cat /etc/passdeposit/mongodb_initial_passdeposit_password.txt
+
+mongodump --db passdeposit -u passdeposit -p "$(cat /etc/passdeposit/mongodb_initial_passdeposit_password.txt)" --out /var/passdeposit/backup/mongo_backup_$(date +%Y-%m-%d)/
