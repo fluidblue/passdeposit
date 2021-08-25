4c301bf0b194fbc450a14b0327865cdc594bdbad
fluidblue <fluidblue@github>
2021-08-25 19:23:16 +0200

    Removed debug output

--- install/scripts/backup.sh
+++ install/scripts/backup.sh
@@ -1,4 +1,1 @@
-echo "Test. Password:"
-cat /etc/passdeposit/mongodb_initial_passdeposit_password.txt
-
 mongodump --host mongodb --db passdeposit -u passdeposit -p "$(cat /etc/passdeposit/mongodb_initial_passdeposit_password.txt)" --out /var/passdeposit/backup/mongo_backup_$(date +%Y-%m-%d)/
