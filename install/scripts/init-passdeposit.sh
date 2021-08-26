5fb208a4a58f1b05469d7dee662cdfc36b936653
fluidblue <fluidblue@github>
2021-08-26 16:44:45 +0200

    Create log file

--- install/scripts/init-passdeposit.sh
+++ install/scripts/init-passdeposit.sh
@@ -3,8 +3,15 @@
 # Exit script immediately if a command exits with a non-zero status.
 set -e
 
+# Define log file location
+LOG_FILE=/passdeposit/log/passdeposit.log
+
 # Install PassDeposit
 npm install --global passdeposit
 
 # Start PassDeposit
-passdeposit --config /passdeposit/config/passdeposit.json
+passdeposit --config /passdeposit/config/passdeposit.json >> "$LOG_FILE" 2>&1 &
+
+# Output backup log as docker container log
+touch "$LOG_FILE"
+tail -n 0 -f "$LOG_FILE"
