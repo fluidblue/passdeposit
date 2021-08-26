f795a54bcc99ca4ec1ae18806181c9e2a4053bb1
fluidblue <fluidblue@github>
2021-08-26 14:50:44 +0200

    Updated backup.sh

--- install/scripts/backup.sh
+++ install/scripts/backup.sh
@@ -1,1 +1,8 @@
-mongodump --host mongodb --db passdeposit -u passdeposit -p "$(cat /etc/passdeposit/mongodb_initial_passdeposit_password.txt)" --out /var/passdeposit/backup/mongo_backup_$(date +%Y-%m-%d)/
+#!/usr/bin/env sh
+
+mongodump \
+	--host mongodb \
+	--db passdeposit \
+	-u passdeposit \
+	-p "$(cat /etc/passdeposit/mongodb_initial_passdeposit_password.txt)" \
+	--out /var/passdeposit/backup/mongo_backup_$(date +%Y-%m-%d)/
