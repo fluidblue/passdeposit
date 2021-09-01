6a1cba9b0204658e9f74b7fc2024511d742b36a0
fluidblue <fluidblue@users.noreply.github.com>
2021-09-01 11:59:33 +0200

    Downgraded MongoDB to v4.4.8 for RPi

--- install/scripts/init-mongodb-backup-anacron.sh
+++ install/scripts/init-mongodb-backup-anacron.sh
@@ -4,11 +4,11 @@
 set -e
 
 # Install mongodb-org-tools according to official docs at
-# https://docs.mongodb.com/manual/tutorial/install-mongodb-on-ubuntu/
+# https://docs.mongodb.com/v4.4/tutorial/install-mongodb-on-ubuntu/
 apt-get update
 apt-get install -y apt-transport-https ca-certificates wget gnupg
-wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 apt-get update
 apt-get install -y mongodb-org-tools="$MONGODB_VERSION"
 
